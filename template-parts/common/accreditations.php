092271e55170ed18634f60af56f2730b08746c53
Alexander Urrutia <matias04041994@gmail.com>
2021-07-06 16:03:02 -0400

    Validate if accreditations

--- template-parts/common/accreditations.php
+++ template-parts/common/accreditations.php
@@ -1,22 +1,22 @@
-<section class="accreditations">
-    <div class="container">
-        <h2 class="accreditations__title">Acreditaciones</h2>
-        <div class="accreditations__slide">
-            <div class="swiper-container">
-                <div class="swiper-wrapper">
-                    <?php $carousel_accreditations = get_field("carousel_accreditations"); ?>
-                    <?php foreach ($carousel_accreditations as $slide) : ?>
-                        <div class="swiper-slide">
-                            <img src="<?= $slide["imagen"]["url"]; ?>" class="img-fluid accreditations__image" />
-                        </div>
-                    <?php endforeach; ?>
+<?php $carousel_accreditations = get_field("carousel_accreditations"); ?>
+<?php if (!empty($carousel_accreditations)) : ?>
+    <section class="accreditations">
+        <div class="container">
+            <h2 class="accreditations__title">Acreditaciones</h2>
+            <div class="accreditations__slide">
+                <div class="swiper-container">
+                    <div class="swiper-wrapper">
+                        <?php foreach ($carousel_accreditations as $slide) : ?>
+                            <div class="swiper-slide">
+                                <img src="<?= $slide["imagen"]["url"]; ?>" class="img-fluid accreditations__image" />
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
+                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-button-next"></div>
                 </div>
-
-                <!-- If we need navigation buttons -->
-                <div class="swiper-button-prev"></div>
-                <div class="swiper-button-next"></div>
+                <slide-accreditations></slide-accreditations>
             </div>
-            <slide-accreditations></slide-accreditations>
         </div>
-    </div>
-</section>
+    </section>
+<?php endif; ?>
