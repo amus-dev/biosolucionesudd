f7b8b0e826866c28a8612509b9ee7fd05d7e1d96
Alexander Urrutia <matias04041994@gmail.com>
2021-08-02 16:32:49 -0400

    Correcciones

--- template-parts/modules/examenes/solicitud.php
+++ template-parts/modules/examenes/solicitud.php
@@ -1,6 +1,8 @@
-<section class="solicitud">
-     <div class="container">
-          <?php $solicitudFile = get_field("solicitud_examenes"); ?>
-          <a href="<?= $solicitudFile; ?>" target="_blank" rel="noopener noreferrer" class="solicitud__btn">Solicitud de exámenes</a>
-     </div>
-</section>
+<?php $solicitudFile = get_field("solicitud_examenes"); ?>
+<?php if (!empty($solicitudFile)) : ?>
+     <section class="solicitud">
+          <div class="container">
+               <a href="<?= $solicitudFile; ?>" target="_blank" rel="noopener noreferrer" class="solicitud__btn">Solicitud de exámenes</a>
+          </div>
+     </section>
+<?php endif; ?>
