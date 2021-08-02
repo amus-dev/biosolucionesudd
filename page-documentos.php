c5c3f41421cd0ed04db3e1352a7d1df7e1f37003
Alexander Urrutia <matias04041994@gmail.com>
2021-08-02 16:26:09 -0400

    Correcciones

--- page-documentos.php
+++ page-documentos.php
@@ -1,5 +1,6 @@
 <?php /* Template Name: Page Documentos */ ?>
 <?php get_header(); ?>
 <?php get_template_part('template-parts/common/title'); ?>
+<?php get_template_part('template-parts/modules/examenes/solicitud'); ?>
 <?php get_template_part('template-parts/modules/examenes/exam'); ?>
 <?php get_footer(); ?>
---
+++ template-parts/modules/examenes/solicitud.php
@@ -0,0 +1,6 @@
+<section class="solicitud">
+     <div class="container">
+          <?php $solicitudFile = get_field("solicitud_examenes"); ?>
+          <a href="<?= $solicitudFile; ?>" target="_blank" rel="noopener noreferrer" class="solicitud__btn">Solicitud de exámenes</a>
+     </div>
+</section>
